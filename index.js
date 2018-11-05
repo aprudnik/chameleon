225208b44eb5e16ea2974e0c66587841b5616d48
Andrejevs <randrejevs@deloittece.com>
2018-11-05 12:42:52 +0200

    Skype test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const verificationControllerFacebook = require('./controllers/verification');
 const verificationControllerSlack = require('./controllers/slackVerification');
+const verificationControllerSkype = require('./controllers/skypeVerification');
 const messageWebhookController = require('./controllers/messageWebhook');
 const discordBot = require('./controllers/discord')
 const getIntents = require('./controllers/pathSelection')
@@ -16,6 +17,11 @@ const dialog = require('./controllers/dialog')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+gText = ""
+function getText(text){
+    gText = text
+    return text
+}
 
 app.get('/', (req, res) => res.send('Hello, I am Chameleon!'))
 app.get('/text', (req, res) => 
@@ -24,6 +30,8 @@ app.get('/text', (req, res) =>
     )) 
 app.get('/verify', verificationControllerFacebook);
 app.post('/slack', verificationControllerSlack);
+app.post('/skype',  (req, res) => {res.send("200"); getText(req.body);return req});//verificationControllerSkype);
+app.get('/skype', (req, res) => res.send(gText))
 app.post('/verify', messageWebhookController);
 app.get('/changeBot/:bot', (req,res) => {
     const params = req.params
